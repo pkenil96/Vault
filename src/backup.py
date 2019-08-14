6a9832725a8cd63c737ddf80c2e0f5f9bfb0ada1
pkenil96 <kenspatel96@gmail.com>
2019-08-14 13:20:02 +0530

    Added virtualenv and enhanced exception handling in backup

--- src/backup.py
+++ src/backup.py
@@ -12,11 +12,13 @@ def upload_single_file_to_s3(filepath, targetName):
         s3.Bucket('securebox.backup').upload_file(filepath, targetName)
         return 0 
     except FileNotFoundError:
-        print('ERROR: File {} was not found'.format(filepath))
+        print('Something went wrong:: {}'.format(filepath))
         return -1
     except NoCredentialsError:
-        print('ERROR: AWS not configured properly')
+        print('Something went wrong:: {}'.format(str(e)))
         return -1
+    except Exception as e:
+        print('Something went wrong:: {}'.format(str(e)))
 
 def get_existing_file_hash():
     s3_cli = boto3.client('s3')
@@ -28,7 +30,7 @@ def get_existing_file_hash():
             etags_list.append(obj['ETag'].strip('"'))
         return etags_list
     except Exception as e:
-        print('ERROR: {}'.format(str(e)))
+        print('Something went wrong:: {}'.format(str(e)))
 
 def check_for_duplicate_files(filepath):
     try:
@@ -37,29 +39,29 @@ def check_for_duplicate_files(filepath):
             return True
         return False
     except FileNotFoundError:
-        print('ERROR: Folder not found')
-
+        print('Something went wrong:: {}'.format(str(e)))
 
 def sync_folder_bucket():
     UPLOADED, FAILED, ALREADY_EXISTS = 0, 0, 0
     try:
         os.chdir(BACKUP_FOLDER_PATH)
+        print('\nBacking up files from: [{}]\n'.format(os.getcwd()))
         all_files = os.listdir('.')
         existing_file_hash = get_existing_file_hash()
         for _file_ in all_files:
             if os.path.isfile(_file_):
                 if calculate_hash(_file_ ) in get_existing_file_hash():
-                    print('[{}] already exists in bucket with a different name.'.format(_file_))
+                    print('ALREADY EXISTS: [{}]'.format(_file_))
                     ALREADY_EXISTS+=1
                 elif upload_single_file_to_s3(_file_, str(_file_)) == 0:
-                    print('[{}] uploaded to the bucket.'.format(str(_file_)))
+                    print('UPLOADED: [{}]'.format(str(_file_)))
                     UPLOADED+=1
                 else:
-                    print('Failed to upload [{}].'.format(str(_file_)))
+                    print('FAILED [{}].'.format(str(_file_)))
                     FAILED+=1
         return UPLOADED, FAILED, ALREADY_EXISTS
-    except FileNotFoundError:
-        print('ERROR: Folder not found')
+    except FileNotFoundError as e:
+        print('Something went wrong:: {}'.format(str(e)))
         sys.exit(-1)
 
 def calculate_hash(filepath):
@@ -68,11 +70,15 @@ def calculate_hash(filepath):
         with open(filepath,"rb") as f:
             hasher.update(f.read()) 
             return hasher.hexdigest()
-    except FileNotFoundError:
-        print('ERROR: Folder not found')
-
+    except FileNotFoundError as e:
+        print('Something went wrong:: {}'.format(str(e)))
 
 if __name__ == '__main__':
+
+    os.system('cls||clear')
+    print('\nUsage:')
+    print('\nFor backup --> python3 backup.py <Folder location>')
+    print('For update --> python3 update.py')
 
     if len(sys.argv) < 2:
         print('To run backup, specify the folder location')
@@ -83,7 +89,6 @@ if __name__ == '__main__':
     #path of the backup folder
     BACKUP_FOLDER_PATH = sys.argv[1]
 
-    print('\nBacking up files from: [{}]\n'.format(BACKUP_FOLDER_PATH))
     up, fail, dup = sync_folder_bucket()
     print('\n*******SUMMARY*******')
     print('UPLOADS: {}'.format(up))
